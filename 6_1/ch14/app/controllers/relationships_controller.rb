aa0d74b21190fe9d2d890845984ac9cc9e95b782
sagawa-hiroki <sagawa.hiroki0@gmail.com>
2022-01-26 16:59:07 +0900

    通知 : フォローされた

--- 6_1/ch14/app/controllers/relationships_controller.rb
+++ 6_1/ch14/app/controllers/relationships_controller.rb
@@ -3,7 +3,18 @@ class RelationshipsController < ApplicationController
 
   def create
     @user = User.find(params[:followed_id])
-    current_user.follow(@user)
+
+    begin
+      ActiveRecord::Base.transaction do
+        current_user.follow(@user)
+        @user.create_user_notification!(Notification::BODY_FOLLOWED, { option_id: current_user.id })
+      end
+    rescue StandardError => e
+      Rails.logger.error e
+      flash[:warning] = "system error"
+      redirect_to(@user) && return
+    end
+
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
