6287c1f4719d4f84378bcc6b0cf69df982ce7324
gestalt0822 <dice.k.aoki@gmail.com>
2022-03-03 18:21:47 +0900

    モデルに関する処理を追加

--- 6_1/ch14/app/models/notification.rb
+++ 6_1/ch14/app/models/notification.rb
@@ -1,2 +1,10 @@
 class Notification < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :user_id, presence: true
+  validates :category, presence: true
+  validates :is_read, inclusion: [true, false]
+
+  enum category: { first_login: 0, followed: 1 }
 end
--- 6_1/ch14/test/models/notification_test.rb
+++ 6_1/ch14/test/models/notification_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class NotificationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @notification = Notification.new(
+      user_id: users(:michael).id,
+      title: 'Your are followed!',
+      category: Notification.categories[:followed],
+      is_read: false
+    )
+  end
+
+  test "should be valid" do
+    assert @notification.valid?
+  end
+
+  test "should require a user_id" do
+    @notification.user_id = nil
+    assert_not @notification.valid?
+  end
+
+  test "should require a title" do
+    @notification.title = nil
+    assert_not @notification.valid?
+  end
+
+  test "should require a category" do
+    @notification.category = nil
+    assert_not @notification.valid?
+  end
 end
